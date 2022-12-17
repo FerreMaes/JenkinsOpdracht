c78299d3c505e44ce942f2d7f1341abda1a46dba
FerreMaes <ferre.maes@student.vives.be>
2022-12-17 13:16:00 +0100

    first commit

--- Index.js
+++
@@ -1,7 +0,0 @@
-const calculator = (expression) => {
-  return eval(expression);
-}
-
-// Example usage:
-console.log(calculator("2 + 3")); // Outputs: 5
-console.log(calculator("(2 + 3) * 5")); // Outputs: 25
